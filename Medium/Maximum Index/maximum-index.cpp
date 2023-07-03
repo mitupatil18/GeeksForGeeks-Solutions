c6875d174d873e1fa5f4d1fa332b7205ef003fd9
Mitali Patil <117500694+mitupatil18@users.noreply.github.com>
2023-07-03 08:35:17 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximum Index/maximum-index.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int maxIndexDiff(int arr[], int n) {
+        int i = 0  , j = n-1 , dif = 0;
+        while(i<j)
+        {
+            if(arr[i]<=arr[j])
+            {
+                dif = max(dif,j-i);
+                i++ ;
+                j = n-1 ;
+            }
+            else
+            j--;
+        }
+        return dif ;
+    }
+
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int a[n];
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        auto ans = ob.maxIndexDiff(a, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
