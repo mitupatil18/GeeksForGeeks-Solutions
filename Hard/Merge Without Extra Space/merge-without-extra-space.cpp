e7b021ed09c4987e77cf714a7ffed6c6da43e69f
Mitali Patil <117500694+mitupatil18@users.noreply.github.com>
2023-07-07 08:59:42 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Merge Without Extra Space/merge-without-extra-space.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+        //Function to merge the arrays.
+        void merge(long long a[], long long b[], int n, int m) 
+        { 
+            // code here 
+            int i = 0  , j = 0 ;
+            while(a[n-1-i]>b[i] && (n-1-i)>=0 && i<m )
+            {
+                swap(a[n-1-i],b[i]);
+                    i++ ;
+            }
+                sort(b,b+m);
+                sort(a,a+n);
+        } 
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    int n, m;
+	    cin >> n >> m;
+	    
+	    long long arr1[n], arr2[m];
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<m;i++){
+	        cin >> arr2[i];
+	    }
+	    Solution ob;
+	    ob.merge(arr1, arr2, n, m); 
+
+        for (int i = 0; i < n; i++) 
+            cout<<arr1[i]<<" "; 
+        
+       
+	    for (int i = 0; i < m; i++) 
+		    cout<<arr2[i]<<" "; 
+	    
+	    cout<<endl;
+	}
+
+	return 0; 
+} 
+
+// } Driver Code Ends
