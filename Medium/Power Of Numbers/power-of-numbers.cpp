47993864ed0424f73656ebe8b7dd1483973ae9a9
Mitali Patil <117500694+mitupatil18@users.noreply.github.com>
2023-07-12 08:44:47 +0530

    Updated Solution - GfG to GitHub

--- Medium/Power Of Numbers/power-of-numbers.cpp
+++ Medium/Power Of Numbers/power-of-numbers.cpp
@@ -5,18 +5,22 @@ using namespace std;
 
 // } Driver Code Ends
 
-
-#define MOD int(1e9 + 7)
 class Solution{
     public:
     //You need to complete this fucntion
     
     long long power(int N,int R)
     {
-       if(R==0)  return 1;
-       long long ans = power(N, R/2);
-       if(R%2==0)  return (ans*ans)%1000000007;
-        else  return (N*((ans*ans)%1000000007)%1000000007);
+       if(R==1)
+       return N ;
+       long MOD = 1000000007;
+       long long ans = power(N,R/2);
+       ans = (ans*ans)%MOD;
+       if(R%2!=0)
+       {
+           ans = (ans*N)%MOD;
+       }
+       return ans ;
     }
 
 };
