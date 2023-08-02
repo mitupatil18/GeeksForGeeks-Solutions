ab0da41e65253e8532e5a8dac7c0d678abd53f5f
Mitali Patil <117500694+mitupatil18@users.noreply.github.com>
2023-08-02 22:04:21 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Count triplets with sum smaller than X/count-triplets-with-sum-smaller-than-x.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	
+	
+	public:
+	long long countTriplets(long long arr[], int n, long long sum)
+	{
+	    // Your code goes here
+	     sort(arr,arr+n);
+	    long long a , b ;
+	    long long i , j , k, cnt = 0 ;
+	    for(i = 0 ; i<n-2 ; i++)
+	    {
+	        j = i+1 ;
+	        k = n-1 ;
+	        while(j<k)
+	        {
+    	        if(arr[i]+arr[j]+arr[k]<sum)
+    	        {
+    	            cnt += (k-j);
+    	            j++ ;
+    	        }
+    	         else if (arr[i]+arr[j]+arr[k]>=sum) k--;
+	        }
+	    }
+	    return cnt ;
+	}
+		 
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+  
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n;
+    	long long sum;
+        cin>>n>>sum;
+        long long arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+	    
+
+       
+
+        Solution ob;
+        cout << ob.countTriplets(arr, n, sum) ;
+	   
+        
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
