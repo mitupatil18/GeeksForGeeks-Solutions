c6f16330cc158ff823e71c91986ff6c92d628b10
Mitali Patil <117500694+mitupatil18@users.noreply.github.com>
2023-08-26 10:14:57 +0530

    Updated Solution - GfG to GitHub

--- Medium/Longest Common Subsequence/longest-common-subsequence.cpp
+++ Medium/Longest Common Subsequence/longest-common-subsequence.cpp
@@ -12,31 +12,21 @@ class Solution
     //Function to find the length of longest common subsequence in two strings.
     int lcs(int n, int m, string s1, string s2)
     {
-       int dp[n+1][m+1];
-       for(int i=0;i<n+1;i++)
-       {
-           for(int j=0;j<m+1;j++)
-           {
-               if(i==0||j==0)
-               {
-                   dp[i][j]=0;
-               }
-           }
-       }
-       for(int i=1;i<n+1;i++)
-       {
-           for(int j=1;j<m+1;j++)
-           {
-               if(s1[i-1]==s2[j-1])
-               {
-                   dp[i][j]=1+dp[i-1][j-1];
-               }
-               else{
-                   dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-               }
-           }
-       }
-       return dp[n][m];
+        int dp[n+1][m+1];
+        int i , j , cnt = 0 ;
+        for(i = 0 ; i<=n ; i++)
+        {
+            for(j = 0 ; j<=m ; j++)
+            {
+                if(i==0 || j==0)
+                dp[i][j] = 0 ;
+                else if(s1[i-1]==s2[j-1])
+                dp[i][j] = dp[i-1][j-1]+1 ;
+                else if(s1[i-1]!=s2[j-1])
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
     }
 };
 
