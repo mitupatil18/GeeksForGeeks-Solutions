e556a47c13853ee3d7f455b4c285f4d96a166f5a
Mitali Patil <mitupatil39@gmail.com>
2023-10-16 22:11:26 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Binary Tree to DLL/binary-tree-to-dll.cpp
@@ -0,0 +1,205 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+    Node(){
+	    left = right = NULL;
+	}
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+
+
+// } Driver Code Ends
+/* Structure for tree and linked list
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+// This function should return head to the DLL
+class Solution
+{
+    public: 
+    //Function to convert binary tree to doubly linked list and return it.
+    void find(Node *a, vector<Node*>& v)
+    {
+        if(a==NULL)return ;
+        find(a->left,v);
+        v.push_back(a);
+        find(a->right,v);
+    }
+    Node * bToDLL(Node *root)
+    {
+        vector<Node*>  v ;
+        find(root,v);
+        int i;
+        if(v.size()==1)
+        {
+            v[0]->left = v[0]->right = NULL ;
+            return v[0] ;
+        }
+        Node * ans = v[0] ;
+        v[0]->left = NULL ;
+        v[0]->right = v[1] ;
+        for(i = 1 ; i<v.size()-1 ; i++)
+        {
+            v[i]->right = v[i+1] ;
+            v[i]->left = v[i-1] ;
+        }
+        v[i]->left = v[i-1] ;
+        v[i]->right = NULL ;
+        return ans ;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+
+/* Function to print nodes in a given doubly linked list */
+void printList(Node *node)
+{
+    Node *prev = NULL;
+    while (node!=NULL)
+    {
+        cout << node->data << " ";
+        prev = node;
+        node = node->right;
+    }
+    cout << endl;
+    while (prev!=NULL)
+    {
+        cout << prev->data << " ";
+        prev = prev->left;
+    }
+    cout << endl;
+}
+
+void inorder(Node *root)
+{
+   if (root != NULL)
+   {
+       inorder(root->left);
+       cout << root->data;
+       inorder(root->right);
+   }
+}
+
+/* Driver program to test size function*/
+int main()
+{
+  int t;
+  cin >> t;
+  getchar();
+  
+  while (t--)
+  {
+     string inp;
+     getline(cin, inp);
+     Node *root = buildTree(inp);
+     
+     Solution ob; 
+     Node *head = ob.bToDLL(root);
+     printList(head);
+
+  }
+  return 0;
+}
+
+
+// } Driver Code Ends
