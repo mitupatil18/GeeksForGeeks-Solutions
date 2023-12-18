6592756feb115ca3e14c5b083ee0876a33fca2bc
Mitali Patil <mitupatil39@gmail.com>
2023-12-18 06:53:41 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Game of XOR/game-of-xor.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int gameOfXor(int n , int arr[]) {
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            int count=(i+1)*(n-i);
+            if(count%2!=0)
+                ans=(ans^(arr[i]));
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+        int A[N];
+        for(int i=0 ; i<N ; i++)
+            cin>>A[i];
+
+        Solution ob;
+        cout << ob.gameOfXor(N,A) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
