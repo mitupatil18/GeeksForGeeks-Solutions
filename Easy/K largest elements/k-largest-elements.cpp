6620179829e8d6e1ea39287d2dfda81c65063868
Mitali Patil <mitupatil39@gmail.com>
2024-03-25 13:39:35 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/K largest elements/k-largest-elements.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to return k largest elements from an array.
+    vector<int> kLargest(int nums[], int n, int k)
+    {
+        priority_queue<int> q;
+        vector<int> v;
+        for(int i = 0 ; i<n ;i++)
+        q.push(nums[i]);
+        while(k>0)
+        {
+            v.push_back(q.top());
+            q.pop();
+            
+            k--;
+        }
+        return v;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n, k;
+        cin >> n >> k;
+        
+        int arr[n];
+        for(int i = 0; i < n;i++)
+            cin>>arr[i];
+        Solution ob;
+        vector<int> result = ob.kLargest(arr, n, k);
+        for (int i = 0; i < result.size(); ++i)
+            cout<<result[i]<<" ";
+        cout << endl;
+        
+    }
+    return 0;
+}
+
+// } Driver Code Ends
