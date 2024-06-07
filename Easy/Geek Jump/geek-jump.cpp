961b2d3f56e5e6c2efe2dfb71c9165def7bf5516
Mitali Patil <mitupatil39@gmail.com>
2024-06-07 09:06:57 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Geek Jump/geek-jump.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int find(vector<int>& dp, vector<int>& h, int n) {
+    if (n == 1)
+        return 0; 
+    if (dp[n] != -1)
+        return dp[n];
+    
+    int oneStep = find(dp, h, n - 1) + abs(h[n - 1] - h[n - 2]);
+    int twoStep = (n > 2) ? find(dp, h, n - 2) + abs(h[n - 1] - h[n - 3]) : INT_MAX;
+    
+    return dp[n] = min(oneStep, twoStep);
+}
+
+int minimumEnergy(vector<int>& h, int n) {
+    vector<int> dp(n + 1, -1);
+    return find(dp, h, n);
+}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<int> arr(N);
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i];
+        }
+        Solution obj;
+        cout << obj.minimumEnergy(arr, N) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
