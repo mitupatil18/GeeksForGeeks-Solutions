cd107c182157423b3c738683c5ed74fe7d7310f6
Mitali Patil <mitupatil39@gmail.com>
2024-06-11 15:09:38 +0530

    Updated Solution - GfG to GitHub

--- Medium/Detect cycle in a directed graph/detect-cycle-in-a-directed-graph.cpp
+++ Medium/Detect cycle in a directed graph/detect-cycle-in-a-directed-graph.cpp
@@ -5,44 +5,35 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-     bool isCyclic(int V, vector<int> adj[]) {
-        // code here
-        
-        int indegree[V] = {0};
-        for(int i = 0; i < V; ++i){
-            for(auto it : adj[i]){
-                indegree[it]++;
-            }
+    // Function to detect cycle in a directed graph.
+    bool isCyclic(int V, vector<int> adj[]) {
+        vector<int> ind(V,0);
+        queue<int> q;
+        vector<int> topo;
+        for(int i = 0 ; i<V ; i++)
+        {
+            for(auto x : adj[i])
+            ind[x]++;
         }
-        queue<int>q;
-        vector<int>ans;
-        
-        for(int i = 0 ; i < V; ++i){
-            if(indegree[i] == 0){
-                q.push(i);
-            }
+        for(int i = 0 ; i<V ; i++)
+        {
+            if(ind[i]==0)
+            q.push(i);
         }
-        
-        while(!q.empty()){
-            int node = q.front();
+        while(!q.empty())
+        {
+            int x = q.front();
             q.pop();
-            ans.push_back(node);
-            
-            for(auto it : adj[node]){
-                indegree[it]--;
-                if(indegree[it] == 0){
-                    q.push(it);
-                }
+            topo.push_back(x);
+            for(auto it : adj[x])
+            {
+                ind[it]--;
+                if(ind[it]==0)
+                q.push(it);
             }
         }
-        if(ans.size() != V){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return topo.size()==V?0:1;
     }
-
 };
 
 //{ Driver Code Starts.
