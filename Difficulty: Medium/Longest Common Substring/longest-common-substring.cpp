743c05d88ed40ed917d8755ca02d00a58940e87f
Mitali Patil <mitupatil39@gmail.com>
2024-08-14 15:47:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
@@ -1,22 +1,25 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
-class Solution{
-    public:
-    
-    int longestCommonSubstr (string t1, string t2, int n, int m)
-    {
-        int ans = 0;
+class Solution {
+  public:
+    int longestCommonSubstr(string str1, string str2) {
+        int n  = str1.size(), m = str2.size();
         vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        for(int i = 1; i<=n;i++)
+        int ans = 0 ;
+        for(int i = 1 ; i<=n ;i++)
         {
-            for(int j = 1 ; j<=m;j++)
+            for(int j = 1 ; j<=m ;j++)
             {
-                if(t1[i-1]==t2[j-1])
-                dp[i][j] = dp[i-1][j-1]+1;
-                ans = max(ans,dp[i][j]);
+                if(str1[i-1]==str2[j-1])
+                {
+                    dp[i][j] = dp[i-1][j-1]+1;
+                    ans = max(ans,dp[i][j]);
+                }
+                
             }
         }
         return ans;
@@ -25,19 +28,15 @@ class Solution{
 
 //{ Driver Code Starts.
 
-int main()
-{
-    int t; cin >> t;
-    while (t--)
-    {
-        int n, m; cin >> n >> m;
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
         string s1, s2;
         cin >> s1 >> s2;
         Solution ob;
 
-        cout << ob.longestCommonSubstr (s1, s2, n, m) << endl;
+        cout << ob.longestCommonSubstr(s1, s2) << endl;
     }
 }
-// Contributed By: Pranay Bansal
-
 // } Driver Code Ends
